e637045b4df8ed2a835b6a571729307db773e7c2
crynobone <crynobone@gmail.com>
2015-10-12 00:52:30 +0800

    Add page title.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,7 @@
 @extends('orchestra/foundation::layouts.main')
 
+@set_meta('title', 'List of Failed Jobs')
+
 @section('content')
 <div class="row">
     <div class="twelve columns white rounded box">
