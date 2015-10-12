86c1d58f2be599ee7d5b735809cefd612b4a748f
crynobone <crynobone@gmail.com>
2015-10-12 09:40:32 +0800

    Add missing abstract method.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/QuemonPlugin.php
+++ src/QuemonPlugin.php
@@ -1,7 +1,9 @@
 <?php namespace Laravie\Quemon;
 
+use Illuminate\Support\Fluent;
 use Orchestra\Extension\Plugin;
 use Laravie\Quemon\Http\Handlers\QuemonMenuHandler;
+use Orchestra\Contracts\Html\Form\Builder as FormBuilder;
 
 class QuemonPlugin extends Plugin
 {
@@ -11,4 +13,17 @@ class QuemonPlugin extends Plugin
      * @var string
      */
     protected $menu = QuemonMenuHandler::class;
+
+    /**
+     * Setup the form.
+     *
+     * @param  \Illuminate\Support\Fluent  $model
+     * @param  \Orchestra\Contracts\Html\Form\Builder  $form
+     *
+     * @return void
+     */
+    protected function form(Fluent $model, FormBuilder $form)
+    {
+        //
+    }
 }
