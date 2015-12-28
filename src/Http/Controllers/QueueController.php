97cf95ca3a848923aa17695045f3bb2c52414e70
crynobone <crynobone@gmail.com>
2015-12-28 15:09:06 +0800

    Run failed-jobs table migrations.
    Closes #1.
    
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Http/Controllers/QueueController.php
+++ src/Http/Controllers/QueueController.php
@@ -1,6 +1,7 @@
 <?php namespace Laravie\Quemon\Http\Controllers;
 
 use Laravie\Quemon\Model\FailedJob;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Artisan;
 use Orchestra\Foundation\Http\Controllers\AdminController;
 
@@ -18,16 +19,45 @@ class QueueController extends AdminController
         $this->middleware('orchestra.csrf', ['only' => 'retry']);
     }
 
+    /**
+     * List of failed jobs.
+     *
+     * @return mixed
+     */
     public function index()
     {
-        $jobs = FailedJob::orderBy('id', 'DESC')->paginate();
+        try {
+            $jobs = FailedJob::orderBy('id', 'DESC')->paginate();
+        } catch (QueryException $e) {
+            return $this->setup();
+        }
 
         return view('laravie/quemon::index', compact('jobs'));
     }
 
+    /**
+     * Push failed job back to queue.
+     *
+     * @param  int  $id
+     *
+     * @return mixed
+     */
     public function retry($id)
     {
         Artisan::call('queue:retry', compact('id'));
+
+        return redirect(handles('orchestra::quemon'));
+    }
+
+    /**
+     * Setup failed jobs migration.
+     *
+     * @return mixed
+     */
+    public function setup()
+    {
+        Artisan::call('queue:failed-table');
+        Artisan::call('migrate');
 
         return redirect(handles('orchestra::quemon'));
     }
